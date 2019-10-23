c72708e777ab969b09ca139247b0f2af9b13171a
Satish Fulwani <36239702+satishf889@users.noreply.github.com>
2019-10-23 18:46:13 +0530

    Adding AdDetail

--- index.js
+++ index.js
@@ -4,100 +4,83 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient({
 });
 
 exports.handler = async (event) => {
-    var body;
-    var likes;
     console.log("Event is ", event)
     var body = JSON.parse(event.body)
     console.log("Body recived: ", body)
-    var user_info = event.requestContext.authorizer.user
-    console.log("User info is :", user_info)
-    user_info = JSON.parse(user_info)
-    var flag = false
-    var ad_username = body.AD_KEY.split("/")[0]
+    var username = body.AD_KEY.split("/")[0]
+    var productDetail;
+    var response
 
-    async function likeCheck1() {
-        console.log("LIKED_ADS are:" + user_info.LIKED_ADS)
-        if (user_info.LIKED_ADS.length == 0) {
-            flag = false
-            return;
-        }
-        console.log("Flag is ", flag)
-        user_info.LIKED_ADS.filter((ad) => {
-            console.log(ad)
-            if (ad == body.AD_KEY) {
-                console.log(ad)
-                console.log("AD Already liked")
-                flag = true
-            }
-        })
-    }
-    await likeCheck1()
-    if (flag == false) {
-        return {
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Content-Type": "application/json",
-            },
-            statusCode: 400,
-            body: "AD is not wishlisted."
-        }
-    }
-    var get_ad_liked_by = {
+    var params = {
         TableName: "ADVERTISEMENT",
         Key: {
-            "USERNAME": ad_username,
+            "USERNAME": username,
             "AD_KEY": body.AD_KEY
         },
-        ProjectionExpression: "LIKED_BY"
+        ProjectionExpression: 'PRODUCT_BRAND,PRODUCT_CATEGORY,PRODUCT_NAME,PRODUCT_DESCRIPTION,PRODUCT_PRICE,PRODUCT_LOCATION,PRODUCT_STATUS,PRODUCT_TIME_POSTED,AD_KEY,S3_LOCATION',
     }
-    await dynamoDb.get(get_ad_liked_by).promise().
-        then(async (res) => {
-            console.log(res.Item.LIKED_BY)
-            var present_array = res.Item.LIKED_BY
-            var position = present_array.indexOf(user_info.USERNAME)
-            console.log("Position in AD Table: " + position)
-            var update_params = {
-                TableName: "ADVERTISEMENT",
-                Key: {
-                    "USERNAME": ad_username,
-                    "AD_KEY": body.AD_KEY
-                },
-                UpdateExpression: "SET LIKES = LIKES - :p REMOVE LIKED_BY[" + position + "]",
-                ExpressionAttributeValues: {
-                    ":p": 1,
-                },
-                ReturnValues: "UPDATED_NEW"
+    await dynamoDb.get(params).promise()
+        .then(async (res) => {
+            console.log("Response after search is: ", res)
+            if (res.Item) {
+                productDetail = res.Item
+                var update_params = {
+                    TableName: "ADVERTISEMENT",
+                    Key: {
+                        "USERNAME": username,
+                        "AD_KEY": body.AD_KEY
+                    },
+                    UpdateExpression: "SET PRODUCT_VIEWS = PRODUCT_VIEWS + :p",
+                    ExpressionAttributeValues: {
+                        ":p": 1,
+                    },
+                    ReturnValues: "UPDATED_NEW"
+                }
             }
-            await dynamoDb.update(update_params).promise().
-                then(async (res) => {
-                    console.log("Likes after update: " + res.Attributes.LIKES)
-                    likes = res.Attributes.LIKES
-                    var user_ad_index = user_info.LIKED_ADS.indexOf(body.AD_KEY)
-                    console.log("Index in user Liked Ads is: " + user_ad_index)
-                    var dislike_ad_user = {
-                        TableName: "USER_INFO",
-                        Key: {
-                            "USERNAME": user_info.USERNAME
-                        },
-                        UpdateExpression: "REMOVE LIKED_ADS[" + user_ad_index + "]",
-                        ReturnValues: "UPDATED_NEW"
+            else {
+                return response = {
+                    headers: {
+                        "Access-Control-Allow-Origin": "*",
+                        "Content-Type": "application/json",
+                    },
+                    statusCode: 400,
+                    body: JSON.stringify("failed")
+                }
+            }
+            console.log("Updated params for ad table are: ", update_params)
+            await dynamoDb.update(update_params).promise()
+                .then((res) => {
+                    console.log("Response after update is: ", res)
+                    if (res.Attributes) {
+                        return {
+                            headers: {
+                                "Access-Control-Allow-Origin": "*",
+                                "Content-Type": "application/json",
+                            },
+                            statusCode: 200,
+                            body: JSON.stringify({ productDetail })
+                        }
                     }
-                    await dynamoDb.update(dislike_ad_user).promise()
-                        .then((res) => {
-                            console.log("User table update response is " + res)
-                        })
+                }).catch((err) => {
+                    console.log("Error is: ", err)
+                    return {
+                        headers: {
+                            "Access-Control-Allow-Origin": "*",
+                            "Content-Type": "application/json",
+                        },
+                        statusCode: 500,
+                        body: "failed"
+                    }
                 })
-                .catch((err) => {
-                    console.log(err)
-                })
+        }).catch((err) => {
+            console.log("Error is: ", err)
+            return {
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Content-Type": "application/json",
+                },
+                statusCode: 400,
+                body: "failed"
+            }
         })
-
-    return {
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json",
-        },
-        statusCode: 200,
-        body: JSON.stringify({ likes })
-    }
-}
+};
--- js/adDetail1.js
+++ js/adDetail1.js
@@ -3,11 +3,64 @@ document.getElementById('closeButton').addEventListener('click', (e) => {
     window.location = 'index1.html'
 })
 
+var getAdDetail=async function(id){
+    var url="https://8xmv9zllk6.execute-api.us-east-1.amazonaws.com/DEV/adinfo"
+    await fetch(url, {
+        Host: "8xmv9zllk6.execute-api.us-east-1.amazonaws.com",
+        method: 'POST',
+        body: JSON.stringify({ "AD_KEY": id }),
+        headers: {
+            'Content-Type': 'application/JSON',
+        }
+    }).then((res) => {
+        if (res.status == 400) {
+            delete_cookie("_sales24JWT")
+            login()
+            return
+        }
+        else if (res.status == 500) {
+            alert("Please try again.")
+            return
+        }
+
+        return res.text().then((data)=>{
+            data=JSON.parse(data)
+            console.log(data.productDetail)
+            let productDetail=data.productDetail
+            document.getElementById('productNameHead').innerText=productDetail.PRODUCT_NAME
+            document.getElementById('productName').innerText=productDetail.PRODUCT_NAME
+            document.getElementById('productPrice').innerText=productDetail.PRODUCT_PRICE
+            document.getElementById('productLocation').innerText=" "+productDetail.PRODUCT_LOCATION
+            document.getElementById('productDescription').innerText=productDetail.PRODUCT_DESCRIPTION
+            document.getElementById('productCategory').innerText=productDetail.PRODUCT_CATEGORY
+            document.getElementById('productBrand').innerText=productDetail.PRODUCT_BRAND
+            document.getElementById('productUsername').innerText=id.split('/')[0]
+            document.getElementById('productImage').src=productDetail.S3_LOCATION
+        })
+    }).catch((err) => {
+        alert("Something went wrong. Please login Again")
+        console.log(err)
+        //delete_cookie("_sales24JWT")
+        //window.location = 'login.html';
+    })
+}
+
+
 const params = new URLSearchParams(document.location.search);
 const adId = params.get("AD_ID");
-// if(adId){
-// adSearch(adId)
-// }
+if(adId){
+getAdDetail(adId)
+}
 // else{
 //     window.location="index1.html"
 // }
+
+if(readCookie('_sales24JWT')!=''){
+    if(likedADS(adId)=="true"){
+        document.getElementById('likeButton').style.color=red
+    }
+}
+
+document.getElementById('likeButton').addEventListener('click',(e)=>{
+    liked(adId)
+})
